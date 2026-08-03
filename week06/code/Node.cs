8229ed89c51a3feb17bad5d1f6b669ab266200d8
wladij <joew.candelario@gmail.com>
2026-08-03 00:17:26 -0500

    week 6 finish

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,10 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data)
+        {
+            return;
+        }
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +38,40 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+    {
+        return true;
     }
+
+    if (value < Data)
+    {
+        if (Left is null)
+            return false;
+
+        return Left.Contains(value);
+    }
+    else
+    {
+        if (Right is null)
+            return false;
+
+        return Right.Contains(value);
+    }
+}
+    
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null)
+            leftHeight = Left.GetHeight();
+
+        if (Right is not null)
+            rightHeight = Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -49,5 +49,17 @@ public static class Trees
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
         // TODO Start Problem 5
+        if (first > last)
+        return;
+
+        int middle = (first + last) / 2;
+
+        bst.Insert(sortedNumbers[middle]);
+
+        // Mitad izquierda
+        InsertMiddle(sortedNumbers, first, middle - 1, bst);
+
+        // Mitad derecha
+        InsertMiddle(sortedNumbers, middle + 1, last, bst);
     }
 }
